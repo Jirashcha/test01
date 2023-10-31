f676896993c4e0f943a750f27e5fe38c07d516ad
Jirashcha <120640294+Jirashcha@users.noreply.github.com>
2023-10-31 23:53:15 +0700

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,28 +16,20 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Fuji.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Fuji background image of mountain with clouds in";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Junior Backend Developer at the Center of Specialty Innovation (CoSI). Currently, I’m a third-year at Bangkok University, Faculty of Information Technology and Innovation (Computer Science - Data Science and Cybersecurity).";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
 
 /**
  * Use this to give more information about what you are passionate about,
